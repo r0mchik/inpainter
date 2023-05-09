166b69ee647ea609eb678d271195485c5d11655f
r0mchik <85110178+r0mchik@users.noreply.github.com>
2023-05-09 14:13:43 +0300

    Update prompt.js

--- pages/prompt.js
+++ pages/prompt.js
@@ -1,1 +1,12 @@
+import { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Canvas from "components/canvas";
+import PromptForm from "components/prompt-form";
+import Dropzone from "components/dropzone";
+import Download from "components/download";
+import { XCircle as StartOverIcon } from "lucide-react";
+import { Code as CodeIcon } from "lucide-react";
+import { Rocket as RocketIcon } from "lucide-react";
 
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
