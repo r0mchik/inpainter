29c91576a4d018e8a2994283965b09296866507a
r0mchik <ka-wa-sa-ki@mail.ru>
2023-05-14 11:53:33 +0300

    retust start next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { Network } = require('lucide-react');
-const withPWA = require("next-pwa");
-const runtimeCaching = require( 'next-pwa/cache.js');
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["replicate.com", "replicate.delivery"],
   },
-  
 }
 
-const config = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  disable: !isProduction,
-  runtimeCaching
-})(
-  nextConfig
-);
-
-module.exports = config
+module.exports = nextConfig
